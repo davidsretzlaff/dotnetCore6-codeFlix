bf5bb91bf7b79f70c694e3e589802f17f81c600c
David da Silva Retzlaff <retzda@compusoftgroup.com>
2023-07-13 21:31:44 -0300

    Creation of CatalogDbContext - Infra.Data.EF

---
+++ src/CodeFlix.Catalog.Infra.Data.EF/CatalogDbContext.cs
@@ -0,0 +1,26 @@
+﻿using CodeFlix.Catalog.Domain.Entity;
+using CodeFlix.Catalog.Infra.Data.EF.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace CodeFlix.Catalog.Infra.Data.EF
+{
+    public class CatalogDbContext : DbContext
+    {
+        public DbSet<Category> Categories => Set<Category>();
+
+        public CatalogDbContext(
+            DbContextOptions<CatalogDbContext> options) 
+            : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // here appies the configuration for all
+            //builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            
+            // here applies the configuration only for what you want
+            // more control
+            builder.ApplyConfiguration(new CategoryConfiguration());
+        }
+    }
+}
