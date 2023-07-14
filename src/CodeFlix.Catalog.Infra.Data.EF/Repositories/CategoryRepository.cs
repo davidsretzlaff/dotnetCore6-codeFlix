36f443fc1bd2b6e9fbe82e3418ee88ce0352a07d
David da Silva Retzlaff <retzda@compusoftgroup.com>
2023-07-13 21:37:46 -0300

    Creation of CategoryRepository - Infra.Data.EF

---
+++ src/CodeFlix.Catalog.Infra.Data.EF/Repositories/CategoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using CodeFlix.Catalog.Domain.Entity;
+using CodeFlix.Catalog.Domain.Repository;
+using CodeFlix.Catalog.Domain.SeedWork.SearchableRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFlix.Catalog.Infra.Data.EF.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly CatalogDbContext _dbContext;
+        private DbSet<Category> _categories => _dbContext.Set<Category>();
+
+        public CategoryRepository(CatalogDbContext dbContext) => _dbContext = dbContext;
+
+        public async Task Insert(Category aggregate, CancellationToken cancelationToken)
+            => await _categories.AddAsync(aggregate, cancelationToken);
+
+        public Task Delete(Category aggregate, CancellationToken cancelationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> Get(Guid id, CancellationToken cancelationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SearchOuput<Category>> Search(SearchInput input, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Category aggregate, CancellationToken cancelationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
