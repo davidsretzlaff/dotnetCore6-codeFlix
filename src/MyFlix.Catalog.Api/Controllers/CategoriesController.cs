c9f519c2b9f6954cd8cbee78b82fe3f67ce7b927
David da Silva Retzlaff <retzda@compusoftgroup.com>
2023-08-04 21:16:40 -0300

    implementing validations errors on update

--- src/MyFlix.Catalog.Api/Controllers/CategoriesController.cs
+++ src/MyFlix.Catalog.Api/Controllers/CategoriesController.cs
@@ -56,6 +56,7 @@ namespace MyFlix.Catalog.Api.Controllers
         [HttpPut("{id:guid}")]
         [ProducesResponseType(typeof(CategoryModelOutput), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status422UnprocessableEntity)]
         public async Task<IActionResult> Update([FromBody] UpdateCategoryInput input, CancellationToken cancellationToken)
         {
             var output = await _mediator.Send(input, cancellationToken);
--- tests/MyFlix.Catalog.EndToEndTest/Api/Category/UpdateCategory/UpdateCategoryApiTest.cs
+++ tests/MyFlix.Catalog.EndToEndTest/Api/Category/UpdateCategory/UpdateCategoryApiTest.cs
@@ -134,5 +134,32 @@ namespace MyFlix.Catalog.EndToEndTest.Api.Category.UpdateCategory
             output.Status.Should().Be((int)StatusCodes.Status404NotFound);
             output.Detail.Should().Be($"Category '{randomGuid}' not found.");
         }
+
+        [Theory(DisplayName = nameof(ErrorWhenCantInstantiateAggregate))]
+        [Trait("EndToEnd/API", "Category/Update - Endpoints")]
+        [MemberData(
+            nameof(UpdateCategoryApiTestDataGenerator.GetInvalidInputs),
+            MemberType = typeof(UpdateCategoryApiTestDataGenerator)
+        )]
+        public async void ErrorWhenCantInstantiateAggregate(UpdateCategoryInput input, string expectedDetail)
+        {
+            var exampleCategoriesList = _fixture.GetExampleCategoriesList(20);
+            await _fixture.Persistence.InsertList(exampleCategoriesList);
+            var exampleCategory = exampleCategoriesList[10];
+            input.Id = exampleCategory.Id;
+
+            var (response, output) = await _fixture.ApiClient.Put<ProblemDetails>(
+                $"/categories/{exampleCategory.Id}",
+                input
+            );
+
+            response.Should().NotBeNull();
+            response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status422UnprocessableEntity);
+            output.Should().NotBeNull();
+            output!.Title.Should().Be("One or more validation errors ocurred");
+            output.Type.Should().Be("UnprocessableEntity");
+            output.Status.Should().Be((int)StatusCodes.Status422UnprocessableEntity);
+            output.Detail.Should().Be(expectedDetail);
+        }
     }
 }
