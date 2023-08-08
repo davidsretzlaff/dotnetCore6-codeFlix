1cd199cfcc90e0512205b7da233757cc1d13e571
David da Silva Retzlaff <retzda@compusoftgroup.com>
2023-08-07 21:12:34 -0300

    creating ListCategories test EndToEnd

---
+++ tests/MyFlix.Catalog.EndToEndTest/Api/Category/ListCategories/ListCategoriesApiTestFixture.cs
@@ -0,0 +1,15 @@
+﻿using MyFlix.Catalog.EndToEndTest.Api.Category.Common;
+using Xunit;
+
+namespace MyFlix.Catalog.EndToEndTest.Api.Category.ListCategories
+{
+
+    [CollectionDefinition(nameof(ListCategoriesApiTestFixture))]
+    public class ListCategoriesApiTestFixtureCollection
+        : ICollectionFixture<ListCategoriesApiTestFixture>
+    { }
+
+    public class ListCategoriesApiTestFixture
+        : CategoryBaseFixture
+    { }
+}
