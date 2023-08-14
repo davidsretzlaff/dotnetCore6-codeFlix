a594dd2bd2bcb130c8d0d0ab982a81ccda5d7cf3
David da Silva Retzlaff <retzda@compusoftgroup.com>
2023-08-14 20:28:34 -0300

    Genre Unit Test - Creating InstantiateWithIsActive test

--- tests/MyFlix.CatalogUnitTests/Domain/Entity/Genre/GenreTest.cs
+++ tests/MyFlix.CatalogUnitTests/Domain/Entity/Genre/GenreTest.cs
@@ -24,5 +24,25 @@ namespace MyFlix.Catalog.UnitTests.Domain.Entity.Genre
             (genre.CreatedAt >= datetimeBefore).Should().BeTrue();
             (genre.CreatedAt <= datetimeAfter).Should().BeTrue();
         }
+
+        [Theory(DisplayName = nameof(Instantiate))]
+        [InlineData(true)]
+        [InlineData(false)]
+        [Trait("Domain", "Genre - Aggregates")]
+        public void InstantiateWithIsActive(bool isActive)
+        {
+            var genreName = "Horror";
+
+            var datetimeBefore = DateTime.Now;
+            var genre = new DomainEntity.Genre(genreName, isActive);
+            var datetimeAfter = DateTime.Now.AddSeconds(1);
+
+            genre.Should().NotBeNull();
+            genre.Name.Should().Be(genreName);
+            genre.IsActive.Should().Be(isActive);
+            genre.CreatedAt.Should().NotBeSameDateAs(default);
+            (genre.CreatedAt >= datetimeBefore).Should().BeTrue();
+            (genre.CreatedAt <= datetimeAfter).Should().BeTrue();
+        }
     }
 }
