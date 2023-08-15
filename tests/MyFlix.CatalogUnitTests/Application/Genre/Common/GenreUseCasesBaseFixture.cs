7bf5fe7a04137d5a1afeba0fda510bee10d5aaa7
David da Silva Retzlaff <retzda@compusoftgroup.com>
2023-08-15 20:52:58 -0300

    Use Cases Genre - Creating CreateGenreTest

---
+++ tests/MyFlix.CatalogUnitTests/Application/Genre/Common/GenreUseCasesBaseFixture.cs
@@ -0,0 +1,10 @@
+﻿using MyFlix.Catalog.UnitTests.Common;
+
+namespace MyFlix.Catalog.UnitTests.Application.Genre.Common
+{
+    public class GenreUseCasesBaseFixture : BaseFixture
+    {
+        public string GetValidGenreName()
+        => Faker.Commerce.Categories(1)[0];
+    }
+}
---
+++ tests/MyFlix.CatalogUnitTests/Application/Genre/CreateGenre/CreateGenreTestFixture.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using MyFlix.Catalog.Application.Interfaces;
+using MyFlix.Catalog.UnitTests.Application.Genre.Common;
+using Xunit;
+
+namespace MyFlix.Catalog.UnitTests.Application.Genre.CreateGenre
+{
+    [CollectionDefinition(nameof(CreateGenreTestFixture))]
+    public class CreateGenreTestFixtureCollection : ICollectionFixture<CreateGenreTestFixture> { }
+    public class CreateGenreTestFixture : GenreUseCasesBaseFixture
+    {
+        public CreateGenreInput GetExampleInput()
+            => new CreateGenreInput(
+                GetValidGenreName(),
+                GetRandomBoolean()
+            );
+        public Mock<IGenreRepository> GetGenreRepositoryMock()
+            => new();
+        public Mock<IUnitOfWork> GetUnitOfWorkMock()
+            => new();
+    }
+}
