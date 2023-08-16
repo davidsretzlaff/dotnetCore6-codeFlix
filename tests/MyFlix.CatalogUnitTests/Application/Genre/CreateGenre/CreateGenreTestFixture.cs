fbf3eb09eacb21352bb71e72cd966932c56d10b7
David da Silva Retzlaff <retzda@compusoftgroup.com>
2023-08-15 21:41:21 -0300

    Use Cases Genre - CreateGenreTest CreateWithRelatedCategories

--- tests/MyFlix.CatalogUnitTests/Application/Genre/CreateGenre/CreateGenreTestFixture.cs
+++ tests/MyFlix.CatalogUnitTests/Application/Genre/CreateGenre/CreateGenreTestFixture.cs
@@ -16,6 +16,19 @@ namespace MyFlix.Catalog.UnitTests.Application.Genre.CreateGenre
                 GetValidGenreName(),
                 GetRandomBoolean()
             );
+
+        public CreateGenreInput GetExampleInputWithCategories()
+        {
+            var numberOfCategoriesIds = (new Random()).Next(1, 10);
+            var categoriesIds = Enumerable.Range(1, numberOfCategoriesIds)
+                .Select(_ => Guid.NewGuid())
+                .ToList();
+            return new(
+                GetValidGenreName(),
+                GetRandomBoolean(),
+                categoriesIds
+            );
+        }
         public Mock<IGenreRepository> GetGenreRepositoryMock()
             => new();
         public Mock<IUnitOfWork> GetUnitOfWorkMock()
