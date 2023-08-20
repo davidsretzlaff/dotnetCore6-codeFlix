c307a1892add0bb5a700e9f26d1e103100866f5b
David da Silva Retzlaff <retzda@compusoftgroup.com>
2023-08-20 17:04:03 -0300

    Use Cases Genre - Creating UpdateGenreTest with related categories

--- tests/MyFlix.CatalogUnitTests/Application/Genre/Common/GenreUseCasesBaseFixture.cs
+++ tests/MyFlix.CatalogUnitTests/Application/Genre/Common/GenreUseCasesBaseFixture.cs
@@ -11,7 +11,17 @@ namespace MyFlix.Catalog.UnitTests.Application.Genre.Common
         public Mock<IUnitOfWork> GetUnitOfWorkMock() => new();
         public Mock<ICategoryRepository> GetCategoryRepositoryMock() => new();
         public string GetValidGenreName() => Faker.Commerce.Categories(1)[0];
-        public DomainEntity.Genre GetExampleGenre(bool? isActive = null)
-            => new( GetValidGenreName(), isActive ?? GetRandomBoolean());
+        public DomainEntity.Genre GetExampleGenre(bool? isActive = null, List<Guid>? categoriesIds = null)
+        {
+            var genre = new DomainEntity.Genre(GetValidGenreName(), isActive ?? GetRandomBoolean());
+            categoriesIds?.ForEach(genre.AddCategory);
+            return genre;
         }
+
+        public List<Guid> GetRandomIdsList(int? count = null)
+            => Enumerable
+                .Range(1, count ?? (new Random()).Next(1, 10))
+                .Select(_ => Guid.NewGuid())
+                .ToList();
+    }
 }
