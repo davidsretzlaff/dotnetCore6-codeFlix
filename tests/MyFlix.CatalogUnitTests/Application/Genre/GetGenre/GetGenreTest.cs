c93e0f88f8df85e737916e5e926039d69a2b0440
David da Silva Retzlaff <retzda@compusoftgroup.com>
2023-08-22 21:27:58 -0300

    Use Cases Genre - Creating GetGenreTest

---
+++ tests/MyFlix.CatalogUnitTests/Application/Genre/GetGenre/GetGenreTest.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Moq;
+using MyFlix.Catalog.Application.UseCases.Genre.Common;
+using MyFlix.Catalog.UnitTests.Domain.Entity.Genre;
+using Xunit;
+
+namespace MyFlix.Catalog.UnitTests.Application.Genre.GetGenre
+{
+    [Collection(nameof(GetGenreTestFixture))]
+    public class GetGenreTest
+    {
+        readonly GetGenreTestFixture _fixture;
+
+        public GetGenreTest(GetGenreTestFixture fixture) => _fixture = fixture;
+        
+        [Fact(DisplayName = nameof(GetGenre))]
+        [Trait("Application", "GetGenre - Use Cases")]
+        public async Task GetGenre()
+        {
+            var genreRepositoryMock = _fixture.GetGenreRepositoryMock();
+            var exampleGenre = _fixture.GetExampleGenre(
+                categoriesIds: _fixture.GetRandomIdsList()
+            );
+            genreRepositoryMock.Setup(x => x.Get(
+                It.Is<Guid>(x => x == exampleGenre.Id),
+                It.IsAny<CancellationToken>()
+            )).ReturnsAsync(exampleGenre);
+            var useCase = new UseCase.GetGenre(genreRepositoryMock.Object);
+            var input = new UseCase.GetGenreInput(exampleGenre.Id);
+
+            GenreModelOutput output = await useCase.Handle(input, CancellationToken.None);
+
+            output.Should().NotBeNull();
+            output.Id.Should().Be(exampleGenre.Id);
+            output.Name.Should().Be(exampleGenre.Name);
+            output.IsActive.Should().Be(exampleGenre.IsActive);
+            output.CreatedAt.Should().BeSameDateAs(exampleGenre.CreatedAt);
+            output.Categories.Should().HaveCount(exampleGenre.Categories.Count);
+            foreach (var expectedId in exampleGenre.Categories)
+                output.Categories.Should().Contain(expectedId);
+            genreRepositoryMock.Verify(
+                x => x.Get(
+                    It.Is<Guid>(x => x == exampleGenre.Id),
+                    It.IsAny<CancellationToken>()
+                ),
+                Times.Once
+            );
+        }
+    }
+}
---
+++ tests/MyFlix.CatalogUnitTests/Application/Genre/GetGenre/GetGenreTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using MyFlix.Catalog.UnitTests.Application.Genre.Common;
+using Xunit;
+
+namespace MyFlix.Catalog.UnitTests.Application.Genre.GetGenre
+{
+    [CollectionDefinition(nameof(GetGenreTestFixture))]
+    public class GetGenreTestFixtureCollection : ICollectionFixture<GetGenreTestFixture> { }
+
+    public class GetGenreTestFixture: GenreUseCasesBaseFixture
+    {
+    }
+}
