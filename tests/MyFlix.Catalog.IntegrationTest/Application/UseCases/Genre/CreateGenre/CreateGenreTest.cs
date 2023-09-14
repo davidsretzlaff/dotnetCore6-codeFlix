b7d6d39042be0ca1a8b38a9bb5d5153738c1db97
David da Silva Retzlaff <retzda@compusoftgroup.com>
2023-09-13 22:56:26 -0300

    Integration/Application - CreateGenre - creating CreateGenre test

---
+++ tests/MyFlix.Catalog.IntegrationTest/Application/UseCases/Genre/CreateGenre/CreateGenreTest.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using MyFlix.Catalog.Application.UseCases.Genre.Common;
+using UseCase = MyFlix.Catalog.Application.UseCases.Genre.CreateGenre;
+using MyFlix.Catalog.Infra.Data.EF.Repositories;
+using MyFlix.Catalog.Infra.Data.EF;
+using Xunit;
+
+namespace MyFlix.Catalog.IntegrationTest.Application.UseCases.Genre.CreateGenre
+{
+    [Collection(nameof(CreateGenreTestFixture))]
+    public class CreateGenreTest
+    {
+        private readonly CreateGenreTestFixture _fixture;
+
+        public CreateGenreTest(CreateGenreTestFixture fixture) => _fixture = fixture;
+
+        [Fact(DisplayName = nameof(CreateGenre))]
+        [Trait("Integration/Application", "CreateGenre - Use Cases")]
+        public async Task CreateGenre()
+        {
+            var input = _fixture.GetExampleInput();
+            var actDbContext = _fixture.CreateDbContext();
+            var createGenre = new UseCase.CreateGenre(
+                new GenreRepository(actDbContext),
+                new UnitOfWork(actDbContext),
+                new CategoryRepository(actDbContext)
+            );
+
+            GenreModelOutput output = await createGenre.Handle(input, CancellationToken.None);
+
+            output.Id.Should().NotBeEmpty();
+            output.Name.Should().Be(input.Name);
+            output.IsActive.Should().Be(input.IsActive);
+            output.CreatedAt.Should().NotBe(default(DateTime));
+            output.Categories.Should().HaveCount(0);
+            var assertDbContext = _fixture.CreateDbContext(true);
+            var genreFromDb = await assertDbContext.Genres.FindAsync(output.Id);
+            genreFromDb.Should().NotBeNull();
+            genreFromDb!.Name.Should().Be(input.Name);
+            genreFromDb.IsActive.Should().Be(input.IsActive);
+        }
+    }
+}
---
+++ tests/MyFlix.Catalog.IntegrationTest/Application/UseCases/Genre/CreateGenre/CreateGenreTestFixture.cs
@@ -0,0 +1,16 @@
+﻿using MyFlix.Catalog.Application.UseCases.Genre.CreateGenre;
+using MyFlix.Catalog.IntegrationTest.Application.UseCases.Genre.Common;
+using Xunit;
+
+namespace MyFlix.Catalog.IntegrationTest.Application.UseCases.Genre.CreateGenre
+{
+    [CollectionDefinition(nameof(CreateGenreTestFixture))]
+    public class CreateGenreTestFixtureCollection : ICollectionFixture<CreateGenreTestFixture>
+    { }
+
+    public class CreateGenreTestFixture : GenreUseCasesBaseFixture
+    {
+        public CreateGenreInput GetExampleInput()
+            => new CreateGenreInput(GetValidGenreName(), GetRandomBoolean());
+    }
+}
