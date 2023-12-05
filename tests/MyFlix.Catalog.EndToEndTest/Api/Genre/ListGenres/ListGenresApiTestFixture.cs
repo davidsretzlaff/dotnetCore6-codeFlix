d3245759456865ac5df2f10111e3d4a8bd73e7b2
David da Silva Retzlaff <retzda@compusoftgroup.com>
2023-12-05 20:25:05 -0300

    Testing the text search feature of ListGenres

--- tests/MyFlix.Catalog.EndToEndTest/Api/Genre/ListGenres/ListGenresApiTestFixture.cs
+++ tests/MyFlix.Catalog.EndToEndTest/Api/Genre/ListGenres/ListGenresApiTestFixture.cs
@@ -1,5 +1,8 @@
 ﻿using MyFlix.Catalog.EndToEndTest.Api.Genre.Common;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
+using DomainEntity = MyFlix.Catalog.Domain.Entity;
 
 namespace MyFlix.Catalog.EndToEndTest.Api.Genre.ListGenres
 {
@@ -11,5 +14,9 @@ namespace MyFlix.Catalog.EndToEndTest.Api.Genre.ListGenres
 
     public class ListGenresApiTestFixture : GenreBaseFixture
     {
-    }
+        public List<DomainEntity.Genre> GetExampleListGenresByNames(List<string> names)
+        {
+            return names.Select(name => GetExampleGenre(name: name)).ToList();
+        }
+	}
 }
