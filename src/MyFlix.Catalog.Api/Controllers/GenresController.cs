88d7230766eb946784503dc47a66f4bd9c780d69
David da Silva Retzlaff <retzda@compusoftgroup.com>
2023-12-05 19:41:37 -0300

    E2E - Implementing the endpoint ListGenres

--- src/MyFlix.Catalog.Api/Controllers/GenresController.cs
+++ src/MyFlix.Catalog.Api/Controllers/GenresController.cs
@@ -6,7 +6,9 @@ using MyFlix.Catalog.Application.UseCases.Genre.Common;
 using MyFlix.Catalog.Application.UseCases.Genre.CreateGenre;
 using MyFlix.Catalog.Application.UseCases.Genre.DeleteGenre;
 using MyFlix.Catalog.Application.UseCases.Genre.GetGenre;
+using MyFlix.Catalog.Application.UseCases.Genre.ListGenres;
 using MyFlix.Catalog.Application.UseCases.Genre.UpdateGenre;
+using MyFlix.Catalog.Domain.SeedWork.SearchableRepository;
 
 namespace MyFlix.Catalog.Api.Controllers
 {
@@ -69,5 +71,28 @@ namespace MyFlix.Catalog.Api.Controllers
             );
             return Ok(new ApiResponse<GenreModelOutput>(output));
         }
-    }
+
+		[HttpGet]
+		[ProducesResponseType(typeof(ListGenresOutput), StatusCodes.Status200OK)]
+		public async Task<IActionResult> List(
+	       CancellationToken cancellationToken,
+	       [FromQuery] int? page = null,
+	       [FromQuery(Name = "per_page")] int? perPage = null,
+	       [FromQuery] string? search = null,
+	       [FromQuery] string? sort = null,
+	       [FromQuery] SearchOrder? dir = null
+        )
+		{
+			var input = new ListGenresInput();
+			if (page is not null) input.Page = page.Value;
+			if (perPage is not null) input.PerPage = perPage.Value;
+			if (!String.IsNullOrWhiteSpace(search)) input.Search = search;
+			if (!String.IsNullOrWhiteSpace(sort)) input.Sort = sort;
+			if (dir is not null) input.Dir = dir.Value;
+
+			var output = await _mediator.Send(input, cancellationToken);
+			return Ok(new ApiResponseList<GenreModelOutput>(output)
+			);
+		}
+	}
 }
--- tests/MyFlix.Catalog.EndToEndTest/Api/Genre/ListGenres/ListGenresApiTest.cs
+++ tests/MyFlix.Catalog.EndToEndTest/Api/Genre/ListGenres/ListGenresApiTest.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using MyFlix.Catalog.Api.ApiModels.Response;
+using MyFlix.Catalog.Application.UseCases.Genre.Common;
 using MyFlix.Catalog.Application.UseCases.Genre.ListGenres;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,17 +29,19 @@ namespace MyFlix.Catalog.EndToEndTest.Api.Genre.ListGenres
             input.Page = 1;
             input.PerPage = exampleGenres.Count;
 
-            var (response, output) = await _fixture.ApiClient.Get<ApiResponse<ListGenresOutput>>("/genres", input);
+            var (response, output) = await _fixture.ApiClient.Get<TestApiResponseList<GenreModelOutput>>("/genres", input);
 
-            response.Should().NotBeNull();
+			response.Should().NotBeNull();
             response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status200OK);
             output.Should().NotBeNull();
-            output!.Data.Total.Should().Be(exampleGenres.Count);
-            output.Data.Items.Count.Should().Be(exampleGenres.Count);
-            output.Data.Page.Should().Be(input.Page);
-            output.Data.PerPage.Should().Be(input.PerPage);
-            output.Data.Items.ToList().ForEach(outputItem =>
-            {
+			output!.Meta.Should().NotBeNull();
+			output.Data.Should().NotBeNull();
+			output.Meta!.Total.Should().Be(exampleGenres.Count);
+			output.Meta.CurrentPage.Should().Be(input.Page);
+			output.Meta.PerPage.Should().Be(input.PerPage);
+			output.Data!.Count.Should().Be(exampleGenres.Count);
+			output.Data.ToList().ForEach(outputItem =>
+			{
                 var exampleItem = exampleGenres.Find(x => x.Id == outputItem.Id);
                 exampleItem.Should().NotBeNull();
                 outputItem.Name.Should().Be(exampleItem!.Name);
