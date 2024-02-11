3289e66c4aa5258fc4269f97fff8e0b9b9fb841e
david retzlaff <david.retzlaff@cyncly.com>
2024-02-11 13:43:17 -0300

    Creating UpdateCastMember test - use case - unit test

---
+++ tests/MyFlix.CatalogUnitTests/Application/CastMember/UpdateCastMember/UpdateCastMemberTest.cs
@@ -0,0 +1,65 @@
+﻿
+using Bogus;
+using FluentAssertions;
+using Moq;
+using MyFlix.Catalog.Application.Interfaces;
+using MyFlix.Catalog.Application.UseCases.CastMember.Common;
+using MyFlix.Catalog.Application.UseCases.Category.UpdateCategory;
+using MyFlix.Catalog.Domain.Entity;
+using MyFlix.Catalog.Domain.Repository;
+using Xunit;
+using UseCase = MyFlix.Catalog.Application.UseCases.CastMember.UpdateCastMember;
+using DomainEntity = MyFlix.Catalog.Domain.Entity;
+namespace MyFlix.Catalog.UnitTests.Application.CastMember.UpdateCastMember
+{
+	[Collection(nameof(UpdateCastMemberTestFixture))]
+	public class UpdateCastMemberTest
+	{
+		private readonly UpdateCastMemberTestFixture _fixture;
+
+		public UpdateCastMemberTest(UpdateCastMemberTestFixture fixture) => _fixture = fixture;
+
+		[Fact(DisplayName = nameof(Update))]
+		[Trait("Application", "UpdateCastMember - Use Cases")]
+		public async Task Update()
+		{
+			var repositoryMock = new Mock<ICastMemberRepository>();
+			var unitOfWorkMock = new Mock<IUnitOfWork>();
+			var castMemberExample = _fixture.GetExampleCastMember();
+			var newName = _fixture.GetValidName();
+			var newType = _fixture.GetType();
+			var input = new UpdatecastMemberInput(castMemberExample.Id, newName,newType);
+			repositoryMock.Setup(x => x.Get(
+				castMemberExample.Id,
+				It.IsAny<CancellationToken>())
+			).ReturnsAsync(castMemberExample);
+			var useCase = new UseCase.UpdateCastMember(repositoryMock.Object, unitOfWorkMock.Object);
+
+			CastMemberModelOutput output = await useCase.Handle(input, CancellationToken.None);
+
+			output.Should().NotBeNull();
+			output.Id.Should().Be(input.Id);
+			output.Name.Should().Be(input.Name);
+			output.Type.Should().Be(input.Type);
+
+			repositoryMock.Verify(x => x.Get(
+				  castMemberExample.Id,
+				It.IsAny<CancellationToken>())
+				, Times.Once);
+
+			repositoryMock.Verify(x => x.Update(
+				It.Is<DomainEntity.CastMember>(
+					x => (
+						x.Id == castMemberExample.Id &&
+						x.Name == input.Name &&
+						x.Type == input.Type
+					)
+				),
+				It.IsAny<CancellationToken>())
+			, Times.Once);
+			unitOfWorkMock.Verify(x => x.Commit(
+				It.IsAny<CancellationToken>())
+			, Times.Once);
+
+		}
+}
---
+++ tests/MyFlix.CatalogUnitTests/Application/CastMember/UpdateCastMember/UpdateCastMemberTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using MyFlix.Catalog.UnitTests.Application.CastMember.Common;
+using Xunit;
+
+namespace MyFlix.Catalog.UnitTests.Application.CastMember.UpdateCastMember
+{
+	[CollectionDefinition(nameof(UpdateCastMemberTestFixture))]
+	public class UpdateCastMemberTestFixtureCollection : ICollectionFixture<UpdateCastMemberTestFixture>
+	public class UpdateCastMemberTestFixture : CastMemberUseCasesBaseFixture
+	{
+	}
+}
