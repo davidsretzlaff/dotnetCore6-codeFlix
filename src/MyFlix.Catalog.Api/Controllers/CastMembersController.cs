11f7424d1bfb3a148d6a14996665124b31e6e274
davidsretzlaff <davids.retzlaff@gmail.com>
2024-03-01 09:25:48 -0300

    API - CatMembers/Create- Endpoints - Implementing Create

--- src/MyFlix.Catalog.Api/Controllers/CastMembersController.cs
+++ src/MyFlix.Catalog.Api/Controllers/CastMembersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MyFlix.Catalog.Api.ApiModels.Response;
 using MyFlix.Catalog.Application.UseCases.CastMember.Common;
+using MyFlix.Catalog.Application.UseCases.CastMember.CreateCastMember;
 using MyFlix.Catalog.Application.UseCases.CastMember.DeleteCastMember;
 using MyFlix.Catalog.Application.UseCases.CastMember.GetCastMember;
 
@@ -32,6 +33,19 @@ namespace MyFlix.Catalog.Api.Controllers
 		{
 			await _mediator.Send(new DeleteCastMemberInput(id), cancellationToken);
 			return NoContent();
+		}
+
+		[HttpPost]
+		[ProducesResponseType(typeof(ApiResponse<CastMemberModelOutput>), StatusCodes.Status201Created)]
+		[ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status422UnprocessableEntity)]
+		public async Task<IActionResult> Create([FromBody] CreateCastMemberInput input, CancellationToken cancellationToken)
+		{
+			var output = await _mediator.Send(input, cancellationToken);
+			return CreatedAtAction(
+				nameof(GetById),
+				new { Id = output.Id },
+				new ApiResponse<CastMemberModelOutput>(output)
+			);
 		}
 	}
 }
