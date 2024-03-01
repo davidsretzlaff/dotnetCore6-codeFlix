73c894f0335bf13baa17aec0aaa6c11bf1ca7a6b
davidsretzlaff <davids.retzlaff@gmail.com>
2024-03-01 09:21:34 -0300

    API - CatMembers/Delete- Endpoints - Implementing Delete

--- src/MyFlix.Catalog.Api/Controllers/CastMembersController.cs
+++ src/MyFlix.Catalog.Api/Controllers/CastMembersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MyFlix.Catalog.Api.ApiModels.Response;
 using MyFlix.Catalog.Application.UseCases.CastMember.Common;
+using MyFlix.Catalog.Application.UseCases.CastMember.DeleteCastMember;
 using MyFlix.Catalog.Application.UseCases.CastMember.GetCastMember;
 
 namespace MyFlix.Catalog.Api.Controllers
@@ -17,10 +18,19 @@ namespace MyFlix.Catalog.Api.Controllers
 
 		[HttpGet("{id:guid}")]
 		[ProducesResponseType(typeof(ApiResponse<CastMemberModelOutput>), StatusCodes.Status200OK)]
+		[ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
 		public async Task<IActionResult> GetById([FromRoute] Guid id, CancellationToken cancellationToken)
 		{
 			var output = await _mediator.Send(new GetCastMemberInput(id), cancellationToken);
 			return Ok(new ApiResponse<CastMemberModelOutput>(output));
+		}
+
+		[HttpDelete("{id:guid}")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		public async Task<IActionResult> Delete([FromRoute] Guid id, CancellationToken cancellationToken)
+		{
+			await _mediator.Send(new DeleteCastMemberInput(id), cancellationToken);
+			return NoContent();
 		}
 	}
 }
