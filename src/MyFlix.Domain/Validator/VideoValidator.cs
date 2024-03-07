d5c259582cedb13ede7cda5e106fd097193d8c00
davidsretzlaff <davids.retzlaff@gmail.com>
2024-03-06 23:56:58 -0300

    Domain - Video Validator - Validators - Creating Implementing validation for title is too long

--- src/MyFlix.Domain/Validator/VideoValidator.cs
+++ src/MyFlix.Domain/Validator/VideoValidator.cs
@@ -6,12 +6,15 @@ namespace MyFlix.Catalog.Domain.Validator
     public class VideoValidator : Validation.Validator
     {
         private readonly Video _video;
+		private const int TitleMaxLength = 255;
 
-        public VideoValidator(Video video, ValidationHandler handler) : base(handler)
+		public VideoValidator(Video video, ValidationHandler handler) : base(handler)
                 => _video = video;
 
         public override void Validate()
         {
-        }
+			if (_video.Title.Length > 255)
+				_handler.HandleError($"'{nameof(_video.Title)}' should be less or equal {TitleMaxLength} characters long");
+		}
     }
 }
