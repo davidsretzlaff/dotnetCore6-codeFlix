f2d94c7f660efaf815def7bddc81bdc9a56993b3
davidsretzlaff <davids.retzlaff@gmail.com>
2024-03-06 23:44:46 -0300

    improving the code

---
+++ src/MyFlix.Domain/Validator/VideoValidator.cs
@@ -0,0 +1,17 @@
+﻿using MyFlix.Catalog.Domain.Entity;
+using MyFlix.Catalog.Domain.Validation;
+
+namespace MyFlix.Catalog.Domain.Validator
+{
+    public class VideoValidator : Validation.Validator
+    {
+        private readonly Video _video;
+
+        public VideoValidator(Video video, ValidationHandler handler) : base(handler)
+                => _video = video;
+
+        public override void Validate()
+        {
+        }
+    }
+}
