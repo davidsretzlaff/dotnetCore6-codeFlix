58d3936757a3e9956beddb8c3ac13cefc23c2b8e
davidsretzlaff <davids.retzlaff@gmail.com>
2024-03-11 22:12:36 -0300

    RatingExtensionsTest

---
+++ src/MyFlix.Domain/Exceptions/RatingExtensions.cs
@@ -0,0 +1,20 @@
+﻿using MyFlix.Catalog.Domain.Enum;
+
+namespace MyFlix.Catalog.Domain.Exceptions
+{
+	public static class RatingExtensions
+	{
+		public static Rating ToRating(this string ratingString)
+			=> ratingString switch
+			{
+				"ER" => Rating.ER,
+				"L" => Rating.L,
+				"10" => Rating.Rate10,
+				"12" => Rating.Rate12,
+				"14" => Rating.Rate14,
+				"16" => Rating.Rate16,
+				"18" => Rating.Rate18,
+				_ => throw new ArgumentOutOfRangeException(nameof(ratingString))
+			};
+	}
+}
---
+++ tests/MyFlix.CatalogUnitTests/Domain/Extensions/RatingExtensionsTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using MyFlix.Catalog.Domain.Enum;
+using MyFlix.Catalog.Domain.Exceptions;
+using Xunit;
+
+namespace MyFlix.Catalog.UnitTests.Domain.Extensions
+{
+	public class RatingExtensionsTest
+	{
+		[Theory(DisplayName = nameof(StringToRating))]
+		[Trait("Domain", "Rating - Extensions")]
+		[InlineData("ER", Rating.ER)]
+		[InlineData("L", Rating.L)]
+		[InlineData("10", Rating.Rate10)]
+		[InlineData("12", Rating.Rate12)]
+		[InlineData("14", Rating.Rate14)]
+		[InlineData("16", Rating.Rate16)]
+		[InlineData("18", Rating.Rate18)]
+		public void StringToRating(string enumString, Rating expectedRating)
+			=> enumString.ToRating().Should().Be(expectedRating);
+
+		[Fact(DisplayName = nameof(ThrowsExceptionWhenInvalidString))]
+		[Trait("Domain", "Rating - Extensions")]
+		public void ThrowsExceptionWhenInvalidString()
+		{
+			var action = () => "Invalid".ToRating();
+			action.Should().Throw<ArgumentOutOfRangeException>();
+		}
+	}
+}
