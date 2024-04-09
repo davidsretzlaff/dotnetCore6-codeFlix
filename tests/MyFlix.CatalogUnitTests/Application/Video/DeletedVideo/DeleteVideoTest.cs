3438e62e352c1963ff6df352f8c999ced8a4b49a
davidsretzlaff <davids.retzlaff@gmail.com>
2024-04-08 21:23:44 -0300

    Setup for testing the DeleteVideo use case

---
+++ tests/MyFlix.CatalogUnitTests/Application/Video/DeletedVideo/DeleteVideoTest.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using MyFlix.Catalog.Application.Interfaces;
+using MyFlix.Catalog.Domain.Repository;
+using Xunit;
+using UseCase = MyFlix.Catalog.Application.UseCases.Video.DeleteVideo;
+
+namespace MyFlix.Catalog.UnitTests.Application.Video.DeletedVideo
+{
+	[Collection(nameof(DeleteVideoTestFixture))]
+	public class DeleteVideoTest
+	{
+		private readonly DeleteVideoTestFixture _fixture;
+		private readonly UseCase.DeleteVideo _useCase;
+		private readonly Mock<IVideoRepository> _repositoryMock;
+		private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+
+		public DeleteVideoTest(DeleteVideoTestFixture fixture)
+		{
+			_fixture = fixture;
+			_repositoryMock = new Mock<IVideoRepository>();
+			_unitOfWorkMock = new Mock<IUnitOfWork>();
+			_useCase = new UseCase.DeleteVideo(
+				_repositoryMock.Object,
+				_unitOfWorkMock.Object
+			);
+		}
+
+		[Fact(DisplayName = nameof(DeleteVideo))]
+		[Trait("Application", "DeleteVideo - Use Cases")]
+		public void DeleteVideo()
+		{
+
+		}
+	}
---
+++ tests/MyFlix.CatalogUnitTests/Application/Video/DeletedVideo/DeleteVideoTestFixture.cs
@@ -0,0 +1,14 @@
+﻿using MyFlix.Catalog.UnitTests.Application.Video.Common.Fixtures;
+using Xunit;
+
+namespace MyFlix.Catalog.UnitTests.Application.Video.DeletedVideo
+{
+	[CollectionDefinition(nameof(DeleteVideoTestFixture))]
+	public class DeleteVideoTestFixtureCollection
+	: ICollectionFixture<DeleteVideoTestFixture>
+	{ }
+
+	public class DeleteVideoTestFixture : VideoTestFixtureBase
+	{
+	}
+}
