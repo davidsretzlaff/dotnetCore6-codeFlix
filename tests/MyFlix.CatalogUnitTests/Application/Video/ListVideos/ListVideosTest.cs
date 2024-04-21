2deb4893f2fb234ea8ee5e345b285652dacf1aab
davidsretzlaff <davids.retzlaff@gmail.com>
2024-04-21 11:46:13 -0300

    Test for ListVideo part I

--- tests/MyFlix.CatalogUnitTests/Application/Video/ListVideos/ListVideosTest.cs
+++ tests/MyFlix.CatalogUnitTests/Application/Video/ListVideos/ListVideosTest.cs
@@ -1,19 +1,44 @@
-﻿using Xunit;
+﻿using Moq;
+using MyFlix.Catalog.Application.Common;
+using MyFlix.Catalog.Domain.Repository;
+using MyFlix.Catalog.Domain.SeedWork.SearchableRepository;
+using Xunit;
+using UseCase = MyFlix.Catalog.Application.UseCases.ListVideos;
 
 namespace MyFlix.Catalog.UnitTests.Application.Video.ListVideos
 {
 	public class ListVideosTest
 	{
 		private readonly ListVideosTestFixture _fixture;
+		private readonly Mock<IVideoRepository> _videoRepositoryMock;
+		private readonly UseCase.ListVideos _useCase;
 
 		public ListVideosTest(ListVideosTestFixture fixture)
-			=> _fixture = fixture;
+		{
+			_fixture = fixture;
+			_videoRepositoryMock = new Mock<IVideoRepository>();
+			_useCase = new UseCase.ListVideos(_videoRepositoryMock.Object);
+		}
 
 		[Fact(DisplayName = nameof(ListVideos))]
 		[Trait("Application", "ListVideos - Use Cases")]
-		public void ListVideos()
+		public async Task ListVideos()
 		{
+			var exampleVideosList = _fixture.CreateExampleVideosList();
+			var input = new ListVideosInput(1, 10, "", "", SearchOrder.Asc);
+			_videoRepositoryMock.Setup(x =>
+				x.Search(
+					It.Is<SearchInput>(x =>
+						x.Page == input.Page &&
+						x.PerPage == input.PerPage &&
+						x.Search == input.Search &&
+						x.OrderBy == input.Sort &&
+						x.Order == input.Dir),
+					It.IsAny<CancellationToken>()
+				).ReturnsAsync(exampleVideosList)
+			);
 
+			PaginatedListOutput<DomainEntities.Video> output = await _useCase.Handle(input, CancellationToken.None);
 		}
 	}
 }
--- tests/MyFlix.CatalogUnitTests/Application/Video/ListVideos/ListVideosTestFixture.cs
+++ tests/MyFlix.CatalogUnitTests/Application/Video/ListVideos/ListVideosTestFixture.cs
@@ -1,6 +1,6 @@
-﻿
-using MyFlix.Catalog.UnitTests.Application.Video.Common.Fixtures;
+﻿using MyFlix.Catalog.UnitTests.Application.Video.Common.Fixtures;
 using Xunit;
+using DomainEntities = MyFlix.Catalog.Domain.Entity;
 
 namespace MyFlix.Catalog.UnitTests.Application.Video.ListVideos
 {
@@ -11,5 +11,9 @@ namespace MyFlix.Catalog.UnitTests.Application.Video.ListVideos
 
 	public class ListVideosTestFixture : VideoTestFixtureBase
 	{
+		public List<DomainEntities.Video> CreateExampleVideosList()
+			=> Enumerable.Range(1, Random.Shared.Next(2, 10))
+				.Select(_ => GetValidVideoWithAllProperties())
+				.ToList();
 	}
 }
