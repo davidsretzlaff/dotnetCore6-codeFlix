e3d9e2cc5045d826beccdf5fa2dedee6808351c3
davidsretzlaff <davids.retzlaff@gmail.com>
2024-04-21 11:43:05 -0300

    Setup for the ListVideos use case tests

---
+++ tests/MyFlix.CatalogUnitTests/Application/Video/ListVideos/ListVideosTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace MyFlix.Catalog.UnitTests.Application.Video.ListVideos
+{
+	public class ListVideosTest
+	{
+		private readonly ListVideosTestFixture _fixture;
+
+		public ListVideosTest(ListVideosTestFixture fixture)
+			=> _fixture = fixture;
+
+		[Fact(DisplayName = nameof(ListVideos))]
+		[Trait("Application", "ListVideos - Use Cases")]
+		public void ListVideos()
+		{
+
+		}
+	}
+}
---
+++ tests/MyFlix.CatalogUnitTests/Application/Video/ListVideos/ListVideosTestFixture.cs
@@ -0,0 +1,15 @@
+﻿
+using MyFlix.Catalog.UnitTests.Application.Video.Common.Fixtures;
+using Xunit;
+
+namespace MyFlix.Catalog.UnitTests.Application.Video.ListVideos
+{
+	[CollectionDefinition(nameof(ListVideosTestFixture))]
+	public class ListVideosTestFixtureCollection
+		: ICollectionFixture<ListVideosTestFixture>
+	{ }
+
+	public class ListVideosTestFixture : VideoTestFixtureBase
+	{
+	}
+}
